96c33b521a092bb5b1e5ab672e0165b5faddddff
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-04 16:29:02 +0530

    #1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 'a' || character == 'e' || character == 'u' || character == 'i' || character == 'o') {
+            System.out.println("Vowel");
+        } else if (character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
